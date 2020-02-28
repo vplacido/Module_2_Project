8e5396a432eb008a9654d9a7d9294cbed78158e4
vplacido <valentinplacido25@gmail.com>
2020-02-28 17:09:35 -0500

    added actions to user and listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,35 @@
 class ListingsController < ApplicationController
+
+    def index
+        @listings = Listing.all
+        # byebug
+    end 
+
+    def show
+        @listing = Listing.find(params[:id])
+    end
+
+    def new
+        @listing = Listing.new
+    end
+
+    def create
+        @listing = Listing.create(params)
+        redirect_to listing_path(@listing)
+    end
+
+    def edit
+        @listing = Listing.find(params[:id])
+    end
+
+    def update
+        @listing = Listing.find(params[:id])
+        @listing.update(params)
+        redirect_to listing_path(@listing)
+    end
+
+    def destroy
+        
+        redirect_to listings_path
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,34 @@ class UsersController < ApplicationController
 
     def index
         @User = User.all
+        # byebug
     end 
 
     def show
         @user = User.find(params[:id])
-    end 
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(params)
+        redirect_to user_path(@user)
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(params)
+        redirect_to user_path(@user)
+    end
+
+    def destroy
+        
+        redirect_to users_path
+    end
 end
