d231aae8e5cc240a8116aafc18de2b55b37aeb60
vplacido <valentinplacido25@gmail.com>
2020-03-02 12:07:16 -0500

    added comment to find if user isa seller

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   # def 
 
   # end 
+
+  # def is seller
+  # go through the services class to find all seller
+  # compare seller_id with this user_id 
+  # return true if the user is a seller
+  # false is user is not a seller and make the user a seller
 end
