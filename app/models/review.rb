4e3f0b20f5d91b483fc76b6336e03dcc130cd2c9
vplacido <valentinplacido25@gmail.com>
2020-03-03 11:50:06 -0500

    added category link to listing show

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,4 @@
 class Review < ApplicationRecord
-<<<<<<< HEAD
     # belongs_to :listing
     belongs_to :service
-=======
-    belongs_to :listing
-    # belongs_to :service
->>>>>>> createHomepage
 end
