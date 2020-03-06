e74e0196efaaadc90f2f76c75abd6a9dbf3bff1d
vplacido <valentinplacido25@gmail.com>
2020-03-06 09:02:11 -0500

    firdayMorning

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,6 +4,5 @@ class ProfileController < ApplicationController
         @listings = Listing.all
         @services = Service.all
         @carts = Cart.all
-        # byebug
     end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,33 +2,6 @@ class Review < ApplicationRecord
     # belongs_to :listing
     belongs_to :service
 
-    # def self.hasReview(service, session_id)
-    #     i = 0
-    #     while i < Review.all.length do 
-    #         # byebug
-    #         if Review.all[i].service_id == service.service_id 
-    #             if Service.find(Review.all[i].service_id).carts.length > 1
-    #                 n = 0
-    #                 while n < Service.find(Review.all[i].service_id).carts.length do 
-    #                     if session_id == Service.find(Review.all[i].service_id).carts[n].user_id
-    #                         return true 
-    #                     end
-    #                     n += 1
-    #                 end
-    #             else
-    #                 if session_id == Service.find(Review.all[i].service_id).carts[0].user_id
-    #                     puts true
-    #                     return true
-    #                 end
-    #             end
-    #             # return false 
-    #         end
-    #         i += 1
-    #     end
-    #     puts false
-    #     return false
-    # end
-
     def self.hasUser(s_id, u_id)
         i = 0
         while i < Review.all.length do
